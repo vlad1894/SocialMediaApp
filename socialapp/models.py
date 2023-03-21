b00007d73b0edc5be8cbadc00857fc87d5d8f1b7
vlad1894 <vladadrian18@yahoo.com>
2023-03-21 20:27:38 +0200

    first commit

---
+++ socialapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        'self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True
+    )
+
+
+# Create your models here.
