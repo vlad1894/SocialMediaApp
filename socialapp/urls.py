6de22ecba6f8fe30af58697503615116afc67d62
vlad1894 <vladadrian18@yahoo.com>
2023-03-22 20:53:08 +0200

    .

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import dashboard, list_of_profiles
+from .views import dashboard, list_of_profiles, profile
 
 app_name = 'thoughts'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('list_of_profiles/', list_of_profiles, name='list_of_profiles')
+    path('list_of_profiles/', list_of_profiles, name='list_of_profiles'),
+    path('profile/<int:pk>', profile, name='profile')
 ]
 
 
--- socialapp/views.py
+++ socialapp/views.py
@@ -9,4 +9,8 @@ def list_of_profiles(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request,'socialapp/list_of_profiles.html', {'profiles': profiles})
 
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, 'socialapp/profile.html', {'profile': profile})
+
 # Create your views here.
