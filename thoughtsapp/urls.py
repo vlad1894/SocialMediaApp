0655afbfcbc91ed005288a151cb6677dbb853bfa
vlad1894 <vladadrian18@yahoo.com>
2023-03-28 20:53:38 +0300

    .

--- thoughtsapp/urls.py
+++ thoughtsapp/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+from userextend.forms import AuthenticationNewForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('socialapp.urls')),
-    path('', include('userextend.urls'))
+    path('', include('userextend.urls')),
+    path('', include("django.contrib.auth.urls")),
+    path('login/', views.LoginView.as_view(form_class=AuthenticationNewForm), name="login")
     
 ]
--- userextend/forms.py
+++ userextend/forms.py
@@ -2,14 +2,14 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
-# class AuthenticationNewForm(AuthenticationForm):
+class AuthenticationNewForm(AuthenticationForm):
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['username'].widget.attrs.update({'class': 'form-control',
-#                                                      'placeholder': 'Please enter your username'})
-#         self.fields['password'].widget.attrs.update({'class': 'form-control',
-#                                                      'placeholder': 'Please enter your password'})
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control',
+                                                     'placeholder': 'Please enter your username'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control',
+                                                     'placeholder': 'Please enter your password'})
 
 
 class UserExtendForm(UserCreationForm):
